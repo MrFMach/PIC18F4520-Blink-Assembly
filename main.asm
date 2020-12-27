0ec4f5435c130aadf8d9b537ac1b9d98231a5b59
MrFMach <fabiosilvamachado@gmail.com>
2020-12-27 06:42:40 -0300

    add comments

--- main.asm
+++ main.asm
@@ -4,45 +4,44 @@
 ;******************************************************************************
 
 ;******************************************************************************
-;Processor directive
+;List directives
 	
-    LIST P=18F4520		;directive to define processor
+    list p=18F4520		;directive to define processor
     
 ;******************************************************************************
 ;Includes
     
-#INCLUDE <P18F4520.INC>	;processor specific variable definitions
+    #INCLUDE <P18F4520.INC>	;processor specific variable definitions
 
 ;******************************************************************************
 ;Configuration bits
 
-    CONFIG	OSC = HS	    ; Pic-board Cristal Oscillator = 20 MHz     
+    CONFIG  OSC = HS		;pic-board cristal cscillator = 20 MHz     
 
 ;******************************************************************************
 ;Reset Vector
 
-RES_VECT  CODE    0x0000            ; processor reset vector
-    GOTO    START                   ; go to beginning of program
+RES_VECT    CODE    0x0000	;processor reset vector
+    GOTO    START		;go to beginning of program
 
 ;******************************************************************************
-;Adicionar interrupções aqui, se for usado
-    
+;Add interrupts here if used
     
 ;******************************************************************************
-;Main
+;Main program
 
-MAIN_PROG CODE                      ; let linker place main program
+MAIN_PROG CODE			;let linker place main program
  
-DCounter1 EQU 0X0C
-DCounter2 EQU 0X0D
-DCounter3 EQU 0X0E
+    DCounter1 EQU 0x0C
+    DCounter2 EQU 0x0D
+    DCounter3 EQU 0x0E
 
 DELAY_1s:
-    MOVLW 0X6d
+    MOVLW 0x6D
     MOVWF DCounter1
-    MOVLW 0X5e
+    MOVLW 0x5E
     MOVWF DCounter2
-    MOVLW 0X1a
+    MOVLW 0x1A
     MOVWF DCounter3
 LOOPD1:
     DECFSZ DCounter1, 1
@@ -54,11 +53,11 @@ LOOPD1:
     RETURN
     
 DELAY_200ms:
-    MOVLW 0Xac
+    MOVLW 0xAC
     MOVWF DCounter1
-    MOVLW 0X13
+    MOVLW 0x13
     MOVWF DCounter2
-    MOVLW 0X06
+    MOVLW 0x06
     MOVWF DCounter3
 LOOPD2:
     DECFSZ DCounter1, 1
@@ -71,21 +70,21 @@ LOOPD2:
     RETURN
 
 START:
-    MOVLW B'00000000'	;TODO O PORTB 
-    MOVWF TRISB		;COMO SAIDA
-    CLRF PORTB		;LIMPA PORTB
+    MOVLW B'00000000'
+    MOVWF TRISB		;PORTB as output
+    CLRF PORTB
     GOTO MAIN
 
 MAIN:
-    BSF PORTB,5		;LIGA BIT 5
-    BSF PORTB,6		;LIGA BIT 6
-    BSF PORTB,7		;LIGA BIT 7
+    BSF PORTB,5		;set bit 5
+    BSF PORTB,6		;set bit 6
+    BSF PORTB,7		;set bit 7
     
     CALL DELAY_200ms
     
-    BCF PORTB,5		;LIGA BIT 5
-    BCF PORTB,6		;LIGA BIT 6
-    BCF PORTB,7		;LIGA BIT 7
+    BCF PORTB,5		;clear bit 5
+    BCF PORTB,6		;clear bit 6
+    BCF PORTB,7		;clear bit 7
     
     CALL DELAY_1s
     
